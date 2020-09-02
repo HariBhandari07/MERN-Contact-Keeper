b168fef6274123719c6eb3aecef2bb32ac97a9db
HariBhandari07 <bhandarihari07@gmail.com>
2020-09-02 16:57:47 +0545

    Edit and Update Contact functionality added

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -68,10 +68,13 @@ const ContactState = props => {
 
     // Clear Current Contact
     const clearCurrent = () => {
-        dispatch({ type: SET_CURRENT })
+        dispatch({ type: CLEAR_CURRENT })
     }
 
     // Update Contact
+    const updateContact = contact => {
+        dispatch({ type: UPDATE_CONTACT, payload: contact })
+    }
 
     // Filter Contact
 
@@ -84,7 +87,8 @@ const ContactState = props => {
             addContact,
             deleteContact,
             setCurrent,
-            clearCurrent
+            clearCurrent,
+            updateContact
         }}>
             {props.children}
         </ContactContext.Provider>
