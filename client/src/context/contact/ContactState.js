3167a4668a855b16246079b822e2a8ba3fb44a7f
HariBhandari07 <bhandarihari07@gmail.com>
2020-09-08 14:14:42 +0545

    Update Contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -62,6 +62,22 @@ const ContactState = props => {
         }
     }
 
+    // Update Contact
+    const updateContact = async contact => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.put(`/api/contacts/${contact._id}`, contact, config)
+            dispatch({ type: UPDATE_CONTACT, payload: res.data })
+        } catch (err) {
+            dispatch({ type: CONTACT_ERROR, payload: err.response.message })
+        }
+        dispatch({ type: UPDATE_CONTACT, payload: contact })
+    }
+
     // Clear Contacts
     const clearContacts = () => {
         dispatch({ type: CLEAR_CONTACTS })
@@ -75,11 +91,6 @@ const ContactState = props => {
     // Clear Current Contact
     const clearCurrent = () => {
         dispatch({ type: CLEAR_CURRENT })
-    }
-
-    // Update Contact
-    const updateContact = contact => {
-        dispatch({ type: UPDATE_CONTACT, payload: contact })
     }
 
     // Filter Contact
